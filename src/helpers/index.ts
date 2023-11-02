bc024b4407da16fae5ca50bdf6e109dc6c4a7dd8
Adrian Townsend <greenlight8004@gmail.com>
2023-11-02 15:09:36 -0400

    feat: added middleware helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import { verifyToken } from '../firebase/index.js';
+import { GraphQLError } from 'graphql';
+
+export const authenticateUser = (userToken: string) => {
+	return new Promise((resolve) => {
+		const tokenVerified = verifyToken(userToken);
+		// throwing a `GraphQLError` here allows us to specify an HTTP status code,
+		// standard `Error`s will have a 500 status code by default
+		if (!tokenVerified)
+			throw new GraphQLError('User is not authenticated', {
+				extensions: {
+					code: 'FORBIDDEN',
+					http: { status: 403 },
+				},
+			});
+		resolve(tokenVerified);
+	});
+};
+
+export const authorizeUser = (apiKey: string) => {
+	return new Promise((resolve) => {
+		// throwing a `GraphQLError` here allows us to specify an HTTP status code,
+		// standard `Error`s will have a 500 status code by default
+		if (apiKey !== process.env.API_KEY)
+			throw new GraphQLError('User is not authorized', {
+				extensions: {
+					code: 'UNAUTHORIZED',
+					http: { status: 401 },
+				},
+			});
+		resolve(true);
+	});
+};
